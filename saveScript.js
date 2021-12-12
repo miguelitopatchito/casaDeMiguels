5e1f35504e6ddbf591f7c583c5465e71b4ab8725
miguelitopatchito <91393821+miguelitopatchito@users.noreply.github.com>
2021-12-12 13:56:20 -0500

    Add files via upload

--- saveScript.js
+++ saveScript.js
@@ -1,6 +1,10 @@
 //saving inputs into an array, still need to populate contacts index from array
-let contactInfoArr = [];
-
+/*
+const contactInfoArr = [];
+//FUNCTION TO NAV TO PROFILE AFTER CLICKING SAVE BUTTON
+const gotime = function go(){
+  window.location.href="contact-profile.html";
+};
 const addCont = (ev)=> {
   ev.preventDefault();
   let contactInfo = {
@@ -12,20 +16,43 @@ const addCont = (ev)=> {
     address: document.getElementById('address').value
   }
   contactInfoArr.push(contactInfo);
-  document.querySelector('form').reset();
+  //CLEAR FORM AFTER SAVE
+  //document.querySelector('form').reset();
 
   console.warn('added', {contactInfoArr});
   //let pre = document.querySelector('#msg pre');
-  //pre.textContent = '\n' + JSON.stringify(contactInfoArr, '\t', 6);
+//  pre.textContent = '\n' + JSON.stringify(contactInfoArr, '\t', 6);
 
   localStorage.setItem('MyContactList', JSON.stringify(contactInfoArr));
 }
 
-function view() {
-    if(localStorage.getItem('MyContactList') != null){
-      document.getElementById('phone-num').innerHTML = contactInfoArr [3];
-    }
-}
+const populate = function view() {
+   if(localStorage.getItem('MyContactList') !== null){
+    document.getElementById('phone-numPro').innerHTML = (contactInfoArr, [2]);
+    document.getElementById('namePro').innerHTML=(contactInfoArr, [1]);
+  }
+};
+
+
+//EVENT LISTENERS ADDING TO ARRAY, NAV TO PROFILE PAGE
+/*
 document.addEventListener ('DOMContentLoaded',  ()=>{
-    document.getElementById('save-button').addEventListener('click', addCont, view);
+  document.getElementById('save-button').addEventListener('click',addCont);
+  document.getElementById('save-button').addEventListener('click', gotime);
 });
+
+*/
+///WHY IN THE HELL DOES THIS NOT WORK
+//window.addEventListener('load', ()=>{
+//		const contactData = localStorage.MyContactList;
+  //              const stringy = JSON.parse(contactData);
+    //  document.getElementById('namePro').innerHTML = '\n' + JSON.stringify(contactInfoArr, '/t', 2);
+              //stringy.contactData.name;
+              //'\n' + JSON.stringify(contactInfoArr, '/t', 2);
+
+
+
+		//});
+
+
+
